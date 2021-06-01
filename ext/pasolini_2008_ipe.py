e8fbe53e9629575c9868c9531d6b746f6fbdaee3
Licia Faenza <licia.faenza@ingv.it>
2021-06-01 09:12:03 +0000

    Replace pasolini_2008_ipe.py
    Former-commit-id: 4fd179b2ec2fa83e56c24e6e0b35cfb7be99c8d8
    Former-commit-id: 5752b89b5d010ae0cbb712ba232acdb11f704f5e

--- ext/pasolini_2008_ipe.py
+++ ext/pasolini_2008_ipe.py
@@ -1,32 +1,32 @@
-# -*- coding: utf-8 -*-
-# vim: tabstop=4 shiftwidth=4 softtabstop=4
+# coding: utf-8
+# The Hazard Library
+# Copyright (C) 2012 GEM Foundation
 #
-# Copyright (C) 2015-2018 GEM Foundation
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General d Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
 #
-# OpenQuake is free software: you can redistribute it and/or modify it
-# under the terms of the GNU Affero General Public License as published
-# by the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# OpenQuake is distributed in the hope that it will be useful,
+# This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
 #
 # You should have received a copy of the GNU Affero General Public License
-# along with OpenQuake. If not, see <http://www.gnu.org/licenses/>.
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+Module exports :
+class:`PasoliniEtAl2008`,
 
 """
-Module exports :class:'PasoliniEtAl2008',
-"""
+from __future__ import division
 import numpy as np
-
-from openquake.hazardlib.gsim.base import IPE, CoeffsTable
+from openquake.hazardlib.gsim.base import CoeffsTable, GMPE
 from openquake.hazardlib import const
 from openquake.hazardlib.imt import MMI
 
 
-class PasoliniEtAl2008(IPE):
+class PasoliniEtAl2008(GMPE):
     """
     Implements the Intensity Prediction Equation of Pasolini Albarello Gasperini
     D'Amico Lolli for MCS Intensity in Italy
@@ -36,38 +36,24 @@ class PasoliniEtAl2008(IPE):
     The coefficient has been reviewed for the seismic hazarda map of Italy in
     ad internal report not published.
 
-    This class implements the version using ipocentral distance, neglecting
-    site amplification
+    This class implements the version using epicentral distancem, with fixed
+    depth, neglecting site amplification
+
+    Model implemented by licia.faenza@ingv.it
     """
-    #: The GMPE is derived from induced earthquakes
     DEFINED_FOR_TECTONIC_REGION_TYPE = const.TRT.ACTIVE_SHALLOW_CRUST
 
-    #: Supported intensity measure types are peak ground acceleration
-    #: and peak ground velocity
-    DEFINED_FOR_INTENSITY_MEASURE_TYPES = set([
-        MMI,
-    ])
+    DEFINED_FOR_INTENSITY_MEASURE_TYPES = {MMI}
 
-    #: Supported intensity measure component is not considered for IPEs, so
-    #: we assume equivalent to 'average horizontal'
-    DEFINED_FOR_INTENSITY_MEASURE_COMPONENT = const.IMC.AVERAGE_HORIZONTAL
+    DEFINED_FOR_INTENSITY_MEASURE_COMPONENT = const.IMC.HORIZONTAL
 
-    #: Supported standard deviation types is total.
-    DEFINED_FOR_STANDARD_DEVIATION_TYPES = set([
-        const.StdDev.TOTAL
-    ])
+    DEFINED_FOR_STANDARD_DEVIATION_TYPES = {const.StdDev.TOTAL}
 
-    #: No required site parameters (in the present version)
     REQUIRES_SITES_PARAMETERS = set()
 
-    #: Required rupture parameters are magnitude (ML is used)
-    REQUIRES_RUPTURE_PARAMETERS = set(('mag', ))
+    REQUIRES_RUPTURE_PARAMETERS = {'mag', 'hypo_depth'}
 
-    #: Required distance measure is rupture distance
-    REQUIRES_DISTANCES = set(('repi',))
-
-
-
+    REQUIRES_DISTANCES = {'repi'}
 
     def get_mean_and_stddevs(self, sites, rup, dists, imt, stddev_types):
         """
@@ -75,20 +61,21 @@ class PasoliniEtAl2008(IPE):
         <.base.GroundShakingIntensityModel.get_mean_and_stddevs>`
         for spec of input and result values.
         """
+        # extract dictionaries of coefficients specific to required
+        # intensity measure type
         C = self.COEFFS[imt]
 
         mean = (self._compute_magnitude_term(C, rup.mag) +
                 self._compute_distance_term(C, dists.repi, rup.mag))
         stddevs = self._get_stddevs(C, dists.repi, stddev_types)
-        return mean, stddevs
 
+        return mean, stddevs
 
     def _compute_magnitude_term(self, C, mag):
         """
         Returns the magnitude scaling term
         """
         return C["m1"] + (C["m2"] * mag)
-
 
     def _compute_distance_term(self, C, repi, mag):
         """
@@ -98,7 +85,6 @@ class PasoliniEtAl2008(IPE):
         D_2 = np.log(D_1)- np.log(C['d2'])
 
         return C["d1"] * (D_1 - C['d2']) + C['d3'] * D_2
-
 
     def _get_stddevs(self, C, distance, stddev_types):
         """
@@ -114,5 +100,5 @@ class PasoliniEtAl2008(IPE):
 
     COEFFS = CoeffsTable(sa_damping=5, table="""
     IMT     m1     m2      d1   d2     d3      s1
-    mmi  -2.466  1.842  -0.0085  4.27  -1.049  0.652584 
+    mmi  -2.466  1.842  -0.0085  4.27  -1.049  0.652584
     """)
