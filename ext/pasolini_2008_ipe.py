0bb072c82c5733c6a0fd65ce28acf5aa2bb16132
Licia Faenza <licia.faenza@ingv.it>
2021-06-02 12:05:30 +0000

    Replace pasolini_2008_ipe.py
    Former-commit-id: eae2e58eafc61b7cf7408885940d6501ea8847cb
    Former-commit-id: b30894c9468c05b1c7aa9fe34d75950ad8bebb3b

--- ext/pasolini_2008_ipe.py
+++ ext/pasolini_2008_ipe.py
@@ -1,27 +1,27 @@
-# -*- coding: utf-8 -*-
-# vim: tabstop=4 shiftwidth=4 softtabstop=4
+# coding: utf-8
+# The Hazard Library
+# Copyright (C) 2012 GEM Foundation
 #
-# Copyright (C) 2015-2021 GEM Foundation
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General d Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
 #
-# OpenQuake is free software: you can redistribute it and/or modify it
-# under the terms of the GNU Affero General Public License as published
-# by the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# OpenQuake is distributed in the hope that it will be useful,
+# This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
 #
 # You should have received a copy of the GNU Affero General Public License
-# along with OpenQuake. If not, see <http://www.gnu.org/licenses/>.
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+Module exports :
+class:`PasoliniEtAl2008`,
 
 """
-Module exports :class:'PasoliniEtAl2008',
-"""
+from __future__ import division
 import numpy as np
-
-from openquake.hazardlib.gsim.base import GMPE, CoeffsTable
+from openquake.hazardlib.gsim.base import CoeffsTable, GMPE
 from openquake.hazardlib import const
 from openquake.hazardlib.imt import MMI
 
@@ -41,31 +41,18 @@ class PasoliniEtAl2008(GMPE):
 
     Model implemented by licia.faenza@ingv.it
     """
-    #: The GMPE is derived from induced earthquakes
     DEFINED_FOR_TECTONIC_REGION_TYPE = const.TRT.ACTIVE_SHALLOW_CRUST
 
-    #: Supported intensity measure types are peak ground acceleration
-    #: and peak ground velocity
-    DEFINED_FOR_INTENSITY_MEASURE_TYPES = set([
-        MMI,
-    ])
+    DEFINED_FOR_INTENSITY_MEASURE_TYPES = {MMI}
 
-    #: Supported intensity measure component is not considered for IPEs, so
-    #: we assume equivalent to 'average horizontal'
-    DEFINED_FOR_INTENSITY_MEASURE_COMPONENT = const.IMC.AVERAGE_HORIZONTAL
+    DEFINED_FOR_INTENSITY_MEASURE_COMPONENT = const.IMC.HORIZONTAL
 
-    #: Supported standard deviation types is total.
-    DEFINED_FOR_STANDARD_DEVIATION_TYPES = set([
-        const.StdDev.TOTAL
-    ])
+    DEFINED_FOR_STANDARD_DEVIATION_TYPES = {const.StdDev.TOTAL}
 
-    #: No required site parameters (in the present version)
     REQUIRES_SITES_PARAMETERS = set()
 
-    #: Required rupture parameters are magnitude (ML is used)
-    REQUIRES_RUPTURE_PARAMETERS = {'mag'}
+    REQUIRES_RUPTURE_PARAMETERS = {'mag', 'hypo_depth'}
 
-    #: Required distance measure is rupture distance
     REQUIRES_DISTANCES = {'repi'}
 
     def get_mean_and_stddevs(self, sites, rup, dists, imt, stddev_types):
@@ -74,10 +61,14 @@ class PasoliniEtAl2008(GMPE):
         <.base.GroundShakingIntensityModel.get_mean_and_stddevs>`
         for spec of input and result values.
         """
+        # extract dictionaries of coefficients specific to required
+        # intensity measure type
         C = self.COEFFS[imt]
+
         mean = (self._compute_magnitude_term(C, rup.mag) +
-                self._compute_distance_term(C, dists.repi))
+                self._compute_distance_term(C, dists.repi, rup.mag))
         stddevs = self._get_stddevs(C, dists.repi, stddev_types)
+
         return mean, stddevs
 
     def _compute_magnitude_term(self, C, mag):
@@ -86,12 +77,12 @@ class PasoliniEtAl2008(GMPE):
         """
         return C["m1"] + (C["m2"] * mag)
 
-    def _compute_distance_term(self, C, repi):
+    def _compute_distance_term(self, C, repi, mag):
         """
         Returns the distance scaling term
         """
         D_1 = np.sqrt(repi**2 + C['d2']**2)
-        D_2 = np.log(D_1) - np.log(C['d2'])
+        D_2 = np.log(D_1)- np.log(C['d2'])
 
         return C["d1"] * (D_1 - C['d2']) + C['d3'] * D_2
 
@@ -104,10 +95,10 @@ class PasoliniEtAl2008(GMPE):
             assert stddev_type in self.DEFINED_FOR_STANDARD_DEVIATION_TYPES
             if stddev_type == const.StdDev.TOTAL:
                 sigma = C["s1"]
-                stddevs.append(sigma + np.zeros_like(distance))
+                stddevs.append(sigma)
         return stddevs
 
     COEFFS = CoeffsTable(sa_damping=5, table="""
-    IMT     m1     m2      d1     d2    d3     s1
-    mmi  -2.466 1.842  -0.0085  4.27  -1.049 0.652584
+    IMT     m1     m2      d1   d2     d3      s1
+    mmi  -2.466  1.842  -0.0085  4.27  -1.049  0.652584
     """)
